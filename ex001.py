59e3b51cd8df72162c4e9f1c3fcedc8e804e2087
arthurpnuness <arthurpessoanunes@gmail.com>
2024-09-03 22:08:35 -0300

    first ex enunciado

---
+++ ex001.py
@@ -0,0 +1,2 @@
+'''Crie um programa que leia o nome, o salário e o tempo de serviço de um funcionário e informe se ele tem direito a receber um aumento. O funcionário deve ter pelo menos 5 anos de serviço e o salário não pode ser superior a R$ 2.000,00 para receber o aumento de 10%. Caso contrário, o aumento é de 5%.'''
+
